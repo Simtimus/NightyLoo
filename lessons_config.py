6623fdbd53c489cd68e809ac7e95ee6620aa82d3
Simtimus <simtimusbos@gmail.com>
2021-11-06 01:57:26 +0200

    Editarea erorilor

--- lessons_config.py
+++ lessons_config.py
@@ -2,14 +2,14 @@ time_table = {
 	'Impara': {
 		'Luni': ['Ed. p. soc', 'L. romana', 'Fizica', 'Ed. fizica'],
 		'Marti': ['Firma E. 2', 'Firma E. 2', 'Electronica I. 1', 'N/A'],
-		'Miercuri': ['Bazele L. D.', 'Matematica', 'L. engleza', 'Informatica', 'Ed. Fizica'],
+		'Miercuri': ['Bazele L. D.', 'Matematica', 'L. engleza', 'Informatica'],
 		'Joi': ['Fizica', 'Electronica I. 1', 'D. P.', 'N/A'],
 		'Vineri': ['L. romana', 'Electronica I. 1', 'Matematica',  'Masini E. A.'],
 	},
 	'Para': {
 		'Luni': ['Geografia', 'L. romana', 'Fizica', 'Ed. fizica'],
 		'Marti': ['Firma E. 2', 'Firma E. 2', 'Electronica I. 1', 'N/A'],
-		'Miercuri': ['Bazele L. D.', 'Matematica', 'L. engleza', 'Informatica', 'Ed. Fizica'],
+		'Miercuri': ['Bazele L. D.', 'Matematica', 'L. engleza', 'Informatica'],
 		'Joi': ['Istoria', 'Electronica I. 1', 'D. P.', 'N/A'],
 		'Vineri': ['Matematica', 'Electronica I. 1', 'Matematica',  'Masini E. A.'],
 	},
